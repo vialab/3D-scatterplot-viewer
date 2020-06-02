8839441de9483d5f0bb1b87e44658d045d0692e0
Anthony <anthony.kouroupis@ontariotechu.net>
2020-06-01 22:48:34 -0400

    uncommented minification line in build-js task

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task("build-js", function()
 		.pipe(source("bundle.js"))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
-		// .pipe(uglify())
+		.pipe(uglify())
 		.pipe(sourcemaps.write("./"))
 		.pipe(gulp.dest("build"))
 		;
