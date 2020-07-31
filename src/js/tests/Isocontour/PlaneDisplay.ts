a2902e474b7b298b6e1282956df8fb7237d3e54c
Anthony Kouroupis <anthony@ubiquenet.com>
2020-07-31 17:12:59 -0400

    Added axis labels to plane display

--- src/js/tests/Isocontour/PlaneDisplay.ts
+++ src/js/tests/Isocontour/PlaneDisplay.ts
@@ -9,6 +9,7 @@ import { InteractableGraph } from "../../io/ui/components/InteractableGraph";
 import { FixedRotationGraph } from "../../io/ui/components/FixedRotationGraph";
 import { Isolines } from "../../io/ui/threejs/Isolines";
 import { WireframeCube } from "../../io/ui/threejs/WireFrameCube";
+import { AxisLabel } from "../../io/ui/threejs/AxisLabel";
 
 export class PlaneDisplay extends TaskDisplay
 {
@@ -19,19 +20,16 @@ export class PlaneDisplay extends TaskDisplay
 	{
 		super();
 
-		let lightColor = new Three.Color(1, 1, 1);
-
-		let interactableGraphBorder = new WireframeCube(axisLength);
-		let orthoGraphBorder = new WireframeCube(axisLength);
-
+		let interactableAxisLabel = new AxisLabel(axisLength);
 		let interactablePlane = GraphPlane.FromPoints(points, axisLength);
 		let orthoPlane = new Isolines(points, axisLength);
 		
-		this.interactableGraph = new InteractableGraph(interactableGraphBorder, interactablePlane, axisLength, new Vector2(0,0), new Three.Vector2(360, 30));
+		this.interactableGraph = new InteractableGraph(interactableAxisLabel, interactablePlane, axisLength, new Vector2(0,0), new Three.Vector2(360, 30));
 		this.interactableGraph.SetAmbientLightStrength(0.5);
 		this.interactableGraph.SetCameraLightStrength(0.5);
 		
-		this.orthoGraph = new FixedRotationGraph(orthoGraphBorder, orthoPlane, axisLength, new Vector2(0,0));
+		let orthoAxisLabel = new AxisLabel(axisLength);
+		this.orthoGraph = new FixedRotationGraph(orthoAxisLabel, orthoPlane, axisLength, new Vector2(0,0));
 		this.orthoGraph.UseOrthographicCamera();
 	}
 
